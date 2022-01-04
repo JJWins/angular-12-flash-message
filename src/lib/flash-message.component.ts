c48a154f3742571822baa80dd3f117ccf3789f31
John Winsley <65803455+JJWins@users.noreply.github.com>
2022-01-04 16:23:31 +0530

    Add files via upload

--- src/lib/flash-message.component.ts
+++ src/lib/flash-message.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit, Input } from '@angular/core';
   template: `
     <div *ngIf="grayOut" [ngClass]="{ grayOut: grayOut }"></div>
     <div
-      class="flash-message alert"
-      [ngClass]="{'alert-primary':alertPrimary,'alert-secondary':alertSecondry,'alert-success':alertSuccess, 'alert-warning': alertWarning, 'alert-danger':alertDanger, 'alert-info':alertInfo, 'alert-light':alertLight, 'alert-dark':alertDark, cssClass: _showContent}"
+      class="flash-message alert {{cssClass}}"
+      [ngClass]="{'alert-primary':alertPrimary,'alert-secondary':alertSecondry,'alert-success':alertSuccess, 'alert-warning': alertWarning, 'alert-danger':alertDanger, 'alert-info':alertInfo, 'alert-light':alertLight, 'alert-dark':alertDark}"
       [hidden]="!_showContent">
       {{ message }}
       <button *ngIf="showCloseBtn"
@@ -17,7 +17,7 @@ import { Component, OnInit, Input } from '@angular/core';
     </div>
   `,
   styles: [
-    '.grayOut {background-color: #333;opacity: 0.7;position: fixed;top: 0px;left: 0px;height: 100%;width: 100%;overflow: hidden;z-index: 9998;} .flash-message{z-index:10000}'
+    '.grayOut {background-color: #333;opacity: 0.7;position: fixed;top: 0px;left: 0px;height: 100%;width: 100%;overflow: hidden;z-index: 9998;} .flash-message{z-index:10000} .flash-message {z-index: 10000; position: fixed; padding: 10px 22px; border-radius: 12px; margin-top: -10px; box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 11px 35px rgba(0,0,0,0.23);} .flash-error {background-color: red; color: white;} .flash-success {background-color: #00bf71;color: white;} .flash-warning {background-color: #bf7100;color: white;}'
   ],
 })
 export class FlashMessageComponent implements OnInit {
